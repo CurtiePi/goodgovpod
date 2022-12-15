f63d83a7491508a3e99ecd6fc37367be2bb4adc1
CurtiePi <curtisbklyn@gmail.com>
2022-12-15 13:42:06 -0500

    Set up the client to use imported packages

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+      <HeaderNav v-if="getLoginApiStatus"  
+        :nav-links="navLinks"/>
+      <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderNav from '@/components/HeaderNavigation'
 
 export default {
   name: 'App',
@@ -22,5 +25,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  height: vh;
+  width: vw;
 }
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
+import vSelect from 'vue-select';
+import 'vue-select/dist/vue-select.css';
 import App from './App.vue'
+import router from './router';
 
-createApp(App).mount('#app')
+import 'devextreme/dist/css/dx.light.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+
+createApp(App)
+    .component('v-select', vSelect)
+    .use(router)
+    .mount('#app')
