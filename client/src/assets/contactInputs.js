fedaaf3f3e753bccab0c25076ef5f214b7edc0d8
CurtiePi <curtisbklyn@gmail.com>
2022-12-15 13:54:31 -0500

    Prepare client assests

---
+++ client/src/assets/contactInputs.js
@@ -0,0 +1,46 @@
+export default {
+  inputFields: [
+    {
+      id: 0,
+      name: 'fname',
+      label: 'First Name',
+      type: 'text',
+      placeholder: 'First Name',
+      value: null
+    },
+    {
+      id: 1,
+      name: 'lname',
+      label: 'Last Name',
+      type: 'text',
+      placeholder: 'Last Name',
+      value: null
+    },
+    {
+      id: 2,
+      name: 'email',
+      label: 'Email',
+      type: 'text',
+      placeholder: 'Email',
+      value: null
+    },
+    {
+      id: 3,
+      name: 'phone',
+      label: 'Phone',
+      type: 'text',
+      placeholder: 'Phone',
+      value: null
+    },
+    {
+      id: 4,
+      name: 'notes',
+      label: 'Contact Notes',
+      type: 'textarea',
+      cols: 60,
+      rows: 5,
+      placeholder: 'Contact Notes',
+      value: null
+    }
+  ]
+}
