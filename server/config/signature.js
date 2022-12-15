37181d3006765812bd66f093a2d3ab02f7adf432
CurtiePi <curtisbklyn@gmail.com>
2022-12-15 10:54:45 -0500

    Added the signature

---
+++ server/config/signature.js
@@ -0,0 +1,23 @@
+let glyph2 = "\n";
+glyph2 += "           <|    .-..-. \n";
+glyph2 += "            |\ \n";
+glyph2 += "           /|.\ \n";
+glyph2 += "          / || \ \n";
+glyph2 += "        ,'  |'  \ \n";
+glyph2 += "     .-'.-==|/_--' \n";
+glyph2 += " ~~~~`--'-------'~~~~~~ \n";
+glyph2 += "~~~~~~~~~~~~~~~~~~~~~~~~"
+//glyph2 += "__--___---___--___---___--___ \n";
+//glyph2 += "___--___---___--___---___--___-__ \n";
+
+const glyph = '~~_/)~~';
+const name = '<img src="cid:dave-signature"/>'
+const email = '<a href="mailto:dave@uksailmakers-ne.com">dave@uksailmakers-ne.com</a>';
+const phone = '646-483-5898';
+const site = '<a href="www.uksailmakers-ne.com">www.uksailmakers-ne.com</a>';
+const embedded = '<img src="cid:dave@uk-sailmakers-ne.com"/>'
+
+const signature = `<pre>${glyph2}</pre><br/>${name}<br/>${email}<br/>${phone}<br/>${site}<br/><br/>${embedded}`;
+
+export default signature;
+
