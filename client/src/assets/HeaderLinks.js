e02e3a0bded5b414afe9a3f8157d9fc316cb4266
CurtiePi <curtisbklyn@gmail.com>
2022-12-15 15:38:21 -0500

    Set up the header links

--- client/src/assets/HeaderLinks.js
+++ client/src/assets/HeaderLinks.js
@@ -1,27 +1,17 @@
 export default [ 
         {
-          text: 'Contact',
-          icon: 'fa-solid fa-person',
-          subNavLinks: [
-            {
-              text: 'Contact List',
-              name: 'Contacts',
-              icon: 'fa-solid fa-people-group',
-            },
-            {
-              text: 'Message Contacts',
-              name: 'SelectContacts',
-              icon: 'fa-solid fa-message'
-            },
-            {
-              text: 'Add Contact',
-              name: 'ContactAdd',
-              icon: 'fa-solid fa-person-circle-plus',
-              param: {
-                name: 'singleOp',
-                value: true
-              }
-            },
-          ]
+          text: 'Contact List',
+          name: 'Contacts',
+          icon: 'fa-solid fa-people-group',
+        },
+        {
+          text: 'Message Contacts',
+          name: 'SelectContacts',
+          icon: 'fa-solid fa-message'
+        },
+        {
+          text: 'Add Contact',
+          name: 'ContactAdd',
+          icon: 'fa-solid fa-person-circle-plus',
         }
       ]
---
+++ client/src/components/HeaderNavigation.vue
@@ -0,0 +1,58 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+    <div class="container-fluid">
+      <router-link class="navbar-brand mr-auto" :to="{ name: 'Home' }" replace><img src="img/icons/sailing_logo.png" height="40" width="41" alt="Sailmakers"/></router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+                <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb2 mb-lg-0">
+          <li class="nav-item dropdown ms-3"
+            v-for="(link, index) in navLinks"
+            :key="index"
+          >
+
+                <router-link class="text-decoration-none" v-else
+                    :to="{ name: link.name }"
+                    :style="{color: '#FFF'}"
+                    replace
+                >
+                  <i :class="link.icon" />
+                    {{ link.text }}
+                </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import headerLinks from '@/assets/HeaderLinks'
+
+export default{
+  props: ['background', 'linkColor', 'hoverBackground'],
+  components: {
+  },
+  data () {
+    return {
+      navLinks: headerLinks 
+    }
+  },
+  methods: {
+    expandTab (event) {
+      event.currentTarget.style.background = this.hoverBackground || '#999'
+      event.currentTarget.lastChild.style.display = 'block'
+    },
+    compressTab (event) {
+      event.currentTarget.style.background = this.background || '#333'
+      event.currentTarget.lastChild.style.display = 'none'
+    }
+  }
+}
+</script>
+<style scoped>
+.dropdown-menu-dark {
+    background-color: teal;
+}
+</style>
