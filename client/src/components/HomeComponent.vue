4b487d845fdd9c2507337019703b5483b71ebd3c
CurtiePi <curtisbklyn@gmail.com>
2022-12-16 15:53:30 -0500

    Updated the splash page

---
+++ client/src/components/HomeComponent.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+    <div class="col-md-12 mt-5">
+      <h1>{{ greeting }}</h1>
+      <p>
+         <router-link :to="{ name: 'ContactList' }">
+           <button>Enter</button>
+         </router-link>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'homeComponent',
+  data () {
+    return {
+      greeting: 'Welcome',
+      msg: 'Good Government Podcast Contacts',
+    }
+  },
+  methods: {
+    createGreeting (name) {
+      var date = new Date()
+      var hour = date.getHours()
+      var timeGreeting = ''
+      if (hour >= 0 && hour < 12) {
+        timeGreeting ='Good Morning' 
+      } else if (hour >= 12 && hour < 18) {
+        timeGreeting ='Good Afternoon' 
+      } else {
+        timeGreeting ='Good Evening' 
+      }
+
+      this.greeting = `${timeGreeting} ${name}`
+    },
+  },
+  mounted () {
+    this.createGreeting("Friend")
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .home {
+    background-color: rgba(0, 176, 234, 0.5);
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .button-container {
+    text-align: center;
+    height: 40px;
+    position: relative;
+    top: -91%;
+    left: -35%;
+  }
+</style>
