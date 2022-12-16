3b7f26d94e63f83f0218b5f526287fc4a8c97c33
CurtiePi <curtisbklyn@gmail.com>
2022-12-16 15:44:03 -0500

    Added pinia to the application

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import vSelect from 'vue-select';
 import 'vue-select/dist/vue-select.css';
 import App from './App.vue'
@@ -8,7 +9,12 @@ import 'devextreme/dist/css/dx.light.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 
-createApp(App)
-    .component('v-select', vSelect)
-    .use(router)
-    .mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+app.use(pinia)
+app.use(router)
+
+router.isReady().then(() => {
+    app.component('v-select', vSelect)
+       .mount('#app')
+})
